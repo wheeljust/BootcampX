ce2731d8d6a6b278bfde3d578c751e83b5715a92
Justin Wheeler <justinwwheeler@gmail.com>
2021-10-02 01:41:27 +0000

    add queries for inner join examples

---
+++ 2_queries_joins/2_cohort_total_assignment_duration.sql
@@ -0,0 +1,5 @@
+SELECT SUM(assignment_submissions.duration) as total_duration
+  FROM assignment_submissions
+  INNER JOIN students ON student_id = students.id
+  INNER JOIN cohorts ON cohort_id = cohorts.id
+  WHERE cohorts.name = 'FEB12';
